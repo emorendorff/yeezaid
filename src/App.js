c5a2ca6a0b7885630f4dc9bc31a6d495445e06ce
Em Orendorff <em.orendorff@gmail.com>
2021-08-04 12:58:57 -0600

    Intial Route set up and landing page jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Landing from './landing/Landing'
+import {Switch, Route, Link, NavLink } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <main>
+    <header>
+      <nav>
+       <Link to='/'> 
+        <h1>Yeezaid What Now?</h1>
+       </Link> 
+       <NavLink className='fav-link' to='/favorites'>My Favorite Yeezaids</NavLink>
+       </nav>
+    </header>
+    <Switch>
+      <Route component={Landing}/>
+    </Switch>
+   </main>
   );
 }
 
---
+++ src/landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = () => {
+    
+    return (
+        <section className='descriptors'>
+        <h2>Satire or Statement? The real Kanye fans will know.</h2>
+        <p>To Play: read the quote and select if you think it's a real or a fake kanye quote. Save the quote for later to reflect on it's inspiration</p>
+        </section>
+    )
+}
+
+export default Landing
