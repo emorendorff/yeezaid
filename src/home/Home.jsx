2258e587ee1f04fca932dc9f6a1803a2a99dbf3f
Em Orendorff <em.orendorff@gmail.com>
2021-08-06 14:49:13 -0600

    Add gradient to main and background styling

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -60,19 +60,16 @@ const Home = () => {
         <div>
             {!trumpQuotes && !kanyeQuotes && !error && isLoading && <h2 className='loading'>Getting quote...</h2> }
             {!trumpQuotes && !kanyeQuotes && error && <h2 className='loading'>Can't load quotes right now, try again later</h2>}
-            {trumpQuotes && kanyeQuotes && !isLoading && !error && randomNum === 1 && <h3 className='quote'>{kanyeQuotes.quote}</h3>}
-            {trumpQuotes && kanyeQuotes && !isLoading && !error && randomNum === 2 && <h3 className='quote'>{trumpQuotes.message}</h3>}
+            {trumpQuotes && kanyeQuotes && !isLoading && !error && randomNum === 1 && <h3 className='quote'>"{kanyeQuotes.quote}"</h3>}
+            {trumpQuotes && kanyeQuotes && !isLoading && !error && randomNum === 2 && <h3 className='quote'>"{trumpQuotes.message}"</h3>}
+            <div className='button-flex'>
+                <button>Ye</button>
+                <button>Ne</button>
+            </div>
+            <button>Add to favorite yeezaids</button>
         </div>
     )
 
 }
 
 export default Home
-
-// {!art.length && <h2 className='loading'>To get started, please select a color from the palette!</h2>}
-// {!art.length && isLoading && !error && <h2 className='loading'>Loading matching paintings 🎨</h2>}
-// {!art.length && error && <h2 className="loading">Something has gone wrong</h2>}
-// {art && !isLoading && !error && <AllPaintings art={art} addToFavorites={addToFavorites}/>}
-
-
-
--- src/landing/Landing.jsx
+++ src/landing/Landing.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Kanye from '../icon_home.png'
+import './landing.css'
 
 const Landing = () => {
     
     return (
         <section className='descriptors'>
+          <img className='kanyeimg' src={Kanye} alt='kanye face icon'/>
           <h2>Satire or Statement? The real Kanye fans will know.</h2>
-          <p>To Play: read the quote and select if you think it's a real or a fake kanye quote. Save the quote for later to reflect on it's inspiration</p>
+          <h3 className='bold'>To Play:</h3>
+          <h4>read the quote and select if you think it's a real or a fake kanye quote. Save the quote for later to reflect on it's inspiration</h4>
           <Link to='/home'>
-            <button>Play</button>
+            <button className='play-btn'>Play</button>
           </Link>
         </section>
     )
