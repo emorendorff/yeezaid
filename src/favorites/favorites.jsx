fe33fde40d18431fe8d82596c2f0d05dca721d85
Em Orendorff <em.orendorff@gmail.com>
2021-08-08 11:55:14 -0600

    Map over favorites array and put in return

--- src/favorites/favorites.jsx
+++ src/favorites/favorites.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Favorites = ({ favorites }) => {
+  let eachFavorite = favorites.map(each => {
+    
+    return <li>{each.quote}</li>})
+
+  
     return (
       <div>
         {!favorites && <h3>No favorites yet!</h3>}
-        {favorites && <p>{favorites.quote}</p>}
+        {favorites && 
+        <ul>
+          {eachFavorite}
+        </ul>}
         <Link to='/home'>
           <button>Back to Home</button>
         </Link>
