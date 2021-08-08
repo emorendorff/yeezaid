23c723809e8f9fc3e47a99e9ffde33dcb4d19e34
Em Orendorff <em.orendorff@gmail.com>
2021-08-08 13:53:09 -0600

    Finish basic testing for landing page

--- cypress/integration/landing.js
+++ cypress/integration/landing.js
@@ -7,4 +7,17 @@ describe('Landing Page', () => {
     cy.get('[alt="kanye face icon"]')
     .should('be.visible')
   })
+
+  it('Should see a title displayed and description of the game', () => {
+    cy.get('h1').contains('Yeezaid What Now?')
+    cy.get('h2').contains('Satire or Statement?')
+  })
+
+  it('Should have a play button to enter the page', () => {
+    cy.get('.play-btn').contains('Play').click()
+  })
+
+  it('Should have a favorites section in navigation', () => {
+    cy.get('.fav-link').contains('My Favorite Yeezaids')
+  })
 })
